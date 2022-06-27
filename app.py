e66786c385d89ee2abad7498253f7b01fe46ed39
Lauro Gomes <lagomes@redhat.com>
2022-06-27 09:33:00 -0300

    Ajustando app

--- app.py
+++ app.py
@@ -35,5 +35,9 @@ def stress():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(
+        host='0.0.0.0', 
+        debug=True, 
+        port=8080
+        )
 
